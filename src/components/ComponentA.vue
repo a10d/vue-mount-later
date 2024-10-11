4de74b3d44fb98eb73912f160baa4ab41c48d068
Aaron Schmied <aaron@schmied.dev>
2024-10-11 12:57:10 +0200

    Add check for interactivity

--- src/components/ComponentA.vue
+++ src/components/ComponentA.vue
@@ -1,11 +1,17 @@
 <script setup>
 
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
+import OtherComponent from "./OtherComponent.vue";
 
 onMounted(() => console.log('component a mounted.'))
 
+const value = ref(1);
 </script>
 
 <template>
   <h1>Komponente A</h1>
+
+  <button @click="value += 1">Interactive? {{ value }}</button>
+
+  <OtherComponent />
 </template>
