4e0cb919edfd52070fceebdd33e8293e895f2194
Aaron Schmied <aaron@schmied.dev>
2024-10-11 12:57:30 +0200

    Restore original template on unmount

--- src/main.js
+++ src/main.js
@@ -1,23 +1,35 @@
 //import {createApp} from 'vue'
-import { createApp } from 'vue/dist/vue.esm-bundler';
+import {createApp} from 'vue/dist/vue.esm-bundler';
 import ComponentA from "./components/ComponentA.vue";
 import ComponentB from "./components/ComponentB.vue";
 
-function mountVue(rootEl) {
+function mountVue(rootSelector) {
+
+    // Template bzw. HTML vom mitgegebenen Element laden
+    const template = document.querySelector(rootSelector)?.innerHTML;
 
     const app = createApp({
-        template: rootEl.innerHTML,
+        template,
         components: {
             ComponentA,
             ComponentB,
         }
     })
 
-    app.mount(rootEl)
-
     // app.use(pinia)
 
-    return app;
+    app.mount(rootSelector)
+
+    // App objekt zurückgeben mit überschriebener unmount mehtode.
+    return {
+        ...app,
+        unmount() {
+            app.unmount()
+
+            // Originales html wiederherstellen
+            document.querySelector(rootSelector).innerHTML = template;
+        }
+    };
 }
 
 //export {mountVue};
